16ea0bab8b193dc4c0f55c5e4dc7f89e6bf3bde5
Serafim Gavrilov <seraf.gavrilov@gmail.com>
2020-06-19 00:08:35 +0300

    fixed timer

--- src/components/clock/index.js
+++
@@ -1,1 +0,0 @@
-export { Clock } from './clock.jsx';
--- src/components/timer/timer.jsx
+++ src/components/timer/timer.jsx
@@ -1,63 +1,71 @@
 import React, { useState, useEffect } from 'react';
 import './timer.css';
 
-import { Control } from '../control';
-
 const Timer = () => {
 
   const initialState = {
-    isReset: false,
     isSession: true,
     isBreak: false,
     isPaused: true,
-    initialSession: 1000 * 60 * 25,
-    initialBreak: 1000 * 60 * 5,
-    sessionTime: 1000 * 60 * 25,
-    breakTime: 1000 * 60 * 5
+    initialSession: 25,
+    initialBreak: 5,
+    count: 25 * 60 * 1000
+  }
+
+  const soundStop = () => {
+    const sound = document.querySelector('#beep');
+    sound.pause();
+    sound.currentTime = 0;
   }
 
   const [timer, setTimer] = useState(initialState);
 
-    useEffect(() => {
-      let interval = null;
+  useEffect(() => {
+    let interval = null;
 
-      if (timer.isPaused) return;
+    const soundStart = () => {
+      const sound = document.querySelector('#beep');
+      sound.currentTime = 0;
+      sound.play();
+    }
 
-      if (timer.isSession) {
-        if (timer.sessionTime > 0) {
-          interval = setInterval(() => {
-            setTimer({ ...timer, sessionTime: timer.sessionTime - 1000 });
-          }, 1000);
-        } else {
-          setTimer({
-            ...timer,
-            isSession: false,
-            isBreak: true,
-            sessionTime: timer.initialSession,
-            breakTime: timer.breakTime - 1000
-          });
-        }
-      } else if (timer.isBreak) {
-        if (timer.breakTime > 0) {
-          interval = setInterval(() => {
-            setTimer({ ...timer, breakTime: timer.breakTime - 1000 });
-          }, 1000);
-        } else {
-          setTimer({
-            ...timer,
-            breakTime: timer.timeBreak,
-            sessionTime: timer.initialSession - 1000,
-            isSession: true,
-            isBreak: false
-          });
-        }
+    if (timer.isPaused) return;
+
+    if (timer.isSession) {
+      if (timer.count > 0) {
+        interval = setInterval(() => {
+          setTimer({ ...timer, count: timer.count - 1000 });
+        }, 1000);
+      } else {
+        setTimer({
+          ...timer,
+          isSession: false,
+          isBreak: true,
+          count: timer.initialBreak * 60 * 1000
+        });
+        soundStart();
       }
+    } else if (timer.isBreak) {
+      if (timer.count > 0) {
+        interval = setInterval(() => {
+          setTimer({ ...timer, count: timer.count - 1000 });
+        }, 1000);
+      } else {
+        setTimer({
+          ...timer,
+          isSession: true,
+          isBreak: false,
+          count: timer.initialSession * 60 * 1000
+        });
+        soundStart();
+      }
+    }
 
     return () => clearInterval(interval);
-  }, [timer, timer.isPaused]);
+  }, [timer]);
 
   const formateDate = (time) => {
-    const isSixty = (time) => time === 3600000 ? '60' : Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)); 
+    const isSixty = (time) => time === 3600000 ? '60' : Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
     const digitFormat = (n) => {
       return ('0' + n).slice(-2);
     }
@@ -67,46 +75,86 @@ const Timer = () => {
     return `${minutes}:${seconds}`;
   }
 
-  const setControlLength = (value, timerValue) => {
-    if (timerValue === 'breakTime') {
-      setTimer({ ...timer, breakTime: value, initialBreak: value });
-    } else {
-      setTimer({ ...timer, sessionTime: value, initialSession: value });
+  const changeLength = (lengthName, operator) => {
+    if (lengthName === 'break' && operator === '+' && timer.initialBreak < 60) {
+      if (timer.isBreak) {
+        return setTimer({ ...timer, initialBreak: timer.initialBreak + 1, count: timer.count + 60000 });
+      }
+      return setTimer({ ...timer, initialBreak: timer.initialBreak + 1 });
+    }
+    if (lengthName === 'break' && operator === '-' && timer.initialBreak > 1) {
+      if (timer.isBreak) {
+        return setTimer({ ...timer, initialBreak: timer.initialBreak - 1, count: timer.count - 60000 });
+      }
+      setTimer({ ...timer, initialBreak: timer.initialBreak - 1 });
+    }
+    if (lengthName === 'session' && operator === '+' && timer.initialSession < 60) {
+      if (timer.isSession) {
+        return setTimer({ ...timer, initialSession: timer.initialSession + 1, count: timer.count + 60000 });
+      }
+      return setTimer({ ...timer, initialSession: timer.initialSession + 1 });
+    }
+    if (lengthName === 'session' && operator === '-' && timer.initialSession > 1) {
+      if (timer.isSession) {
+        return setTimer({ ...timer, initialSession: timer.initialSession - 1, count: timer.count - 60000 });
+      }
+      return setTimer({ ...timer, initialSession: timer.initialSession - 1 });
     }
   }
 
   return (
     <div className="timer">
-      <Control
-        name="break"
-        paused={timer.isPaused}
-        isReset={timer.isReset}
-        value={timer.initialBreak / (1000 * 60)}
-        onChangeLength={(value) => setControlLength(value, 'breakTime')}
-      />
-      <Control
-        name="session"
-        paused={timer.isPaused}
-        isReset={timer.isReset}
-        value={timer.initialSession / (1000 * 60)}
-        onChangeLength={(value) => setControlLength(value, 'sessionTime')}
-      />
+      <div className="length-control">
+        <div id="break-label">
+          Break length
+        </div>
+        <div className="length-control--settings">
+          <button className="increment-button btn-level" id="break-increment" onClick={() => changeLength('break', '+')}>
+            <i className="fa fa-arrow-up"></i>
+          </button>
+          <div id="break-length">{timer.initialBreak}</div>
+          <button className="decrement-button btn-level" id="break-decrement" onClick={() => changeLength('break', '-')}>
+            <i className="fa fa-arrow-down"></i>
+          </button>
+        </div>
+      </div>
+      <div className="length-control">
+        <div id="session-label">
+          Session length
+        </div>
+        <div className="length-control--settings">
+          <button className="increment-button btn-level" id="session-increment" onClick={() => changeLength('session', '+')}>
+            <i className="fa fa-arrow-up"></i>
+          </button>
+          <div id="session-length">{timer.initialSession}</div>
+          <button className="decrement-button btn-level" id="session-decrement" onClick={() => changeLength('session', '-')}>
+            <i className="fa fa-arrow-down"></i>
+          </button>
+        </div>
+      </div>
       <div className="timer-wrapper">
         <div id="timer-label">{timer.isSession ? 'Session' : 'Break'}</div>
-        <div id="time-left">{formateDate(timer.isSession ? timer.sessionTime : timer.breakTime)}</div>
+        <div id="time-left">{formateDate(timer.count)}</div>
       </div>
       <div className="timer-control">
         <button
+          className="btn-level"
           id="start_stop"
           onClick={() => setTimer(prevValue => ({ ...prevValue, isPaused: !prevValue.isPaused }))}>
-          Start/Stop
+          <i class="fa fa-play"></i>
+          <i class="fa fa-pause"></i>
         </button>
         <button
+          className="btn-level"
           id="reset"
-          onClick={() => setTimer(initialState)}>
-          Reset
+          onClick={() => {
+            soundStop();
+            setTimer(initialState)
+          }}>
+          <i class="fa fa-refresh"></i>
         </button>
       </div>
+      <audio id="beep" src="https://raw.githubusercontent.com/freeCodeCamp/cdn/master/build/testable-projects-fcc/audio/BeepSound.wav"></audio>
     </div>
   );
 }
